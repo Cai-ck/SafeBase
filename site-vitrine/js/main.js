a9e5628e26eb9ce4a7ff756a2c6f20e3b53f30ce
Caíque <caiquecunhaluiz0@gmail.com>
2026-05-09 13:42:35 +0000

    chore: Limpeza do projeto e adição do código.

---
+++ site-vitrine/js/main.js
@@ -0,0 +1,45 @@
+fetch("assets/data/games.json")
+
+  .then(response => response.json())
+
+  .then(data => {
+
+    const games = data.games;
+
+    const container = document.getElementById("games-container");
+
+    games.forEach(game => {
+
+      container.innerHTML += `
+
+        <div class="game-card">
+
+          <img src="${game.cover}" alt="${game.title}">
+
+          <div class="game-info">
+
+            <h3>${game.title}</h3>
+
+            <p>${game.status.review}</p>
+
+            <div class="genres">
+
+              ${game.genres.map(genre =>
+                `<span>${genre}</span>`
+              ).join("")}
+
+            </div>
+
+          </div>
+
+        </div>
+
+      `;
+
+    });
+
+  })
+
+  .catch(error => {
+    console.error("Erro ao carregar games.json:", error);
+  });
